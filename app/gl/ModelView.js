cc2ebb2c8f5975e49b0c3524079aee137d22641e
Zolat Eater <zolateater@gmail.com>
2016-04-21 00:37:37 +0700

    closest plane changed

--- app/gl/ModelView.js
+++ app/gl/ModelView.js
@@ -157,8 +157,6 @@ ModelView.prototype.startRender = function () {
     gl.enableVertexAttribArray(texCoordAttributeLocation);
 
 
-
-
     // Матрицы - местоположение в шейдерах
     var matWorldUniformLocation = gl.getUniformLocation(program, 'mWorld');
     var matViewUniformLocation = gl.getUniformLocation(program, 'mView');
@@ -170,9 +168,9 @@ ModelView.prototype.startRender = function () {
     var projectionMatrix = new Float32Array(16);
     mat4.identity(worldMatrix);
     // Позиция наблюдателя, куда он смотрит, плюс вектор верха
-    mat4.lookAt(viewMatrix, [0, 0, -10], [0, 0, 0], [0, 1, 0]);
+    mat4.lookAt(viewMatrix, [0, 0, -5], [0, 0, 0], [0, 1, 0]);
     // Поле обзора (в радианах), viewport, closest plane, far plane
-    mat4.perspective(projectionMatrix, glMatrix.toRadian(45), this.canvas.width / this.canvas.height, 0.1, 1000.0);
+    mat4.perspective(projectionMatrix, glMatrix.toRadian(30), this.canvas.width / this.canvas.height, 0.001, 10.0);
 
     // Какую шейдерную программу используем
     gl.useProgram(program);
